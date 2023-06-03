814b6e0e46c1045f7f67c6ee6ab155afcb399607
tak <109452865+taku10101@users.noreply.github.com>
2023-06-04 02:19:04 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import ImgGallery from "./ImgGallery";
 
@@ -12,11 +12,10 @@ function App() {
   const handleSubmit =(e)=>{
     // enter時の時の自動リロードを防ぐ
     e.preventDefault();
-
-    //APIURL
+   //APIURL
     const API_KEY = "36911322-c35cd3a113aaec182e43669d2";
     //クエリパラメータ　inputで受け取ったものをクエリに埋め込む
-    const URL = "https://pixabay.com/api/?key=" + API_KEY + "&q=" + {InputText} //inputから値を取得して埋め込む
+    const URL = "https://pixabay.com/api/?key=" + API_KEY + "&q=" + inputText //inputから値を取得して埋め込む
     //非同期
     fetch(URL)
     .then((res)=>{
@@ -28,12 +27,17 @@ function App() {
     });
   };
 
+  let handleChange=e=>setInputText(e.target.value)
+  
+  
+  
+
   return (
     <>
       <div className='container'>
-        <h2>pix</h2>  
+      <h2>pix</h2>  
         <form onSubmit={(e)=>handleSubmit(e)}>
-          <inpue type='text'　value={InputText}　 placeholder='画像を探す'/>
+          <input type='text' value={inputText}  onChange={handleChange} placeholder='画像を探す'/>
         </form>
         {/* コンポーネントにデータを渡す */}
         <ImgGallery fetchData={fetchData} />
